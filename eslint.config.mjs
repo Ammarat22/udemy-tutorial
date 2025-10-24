d8cd5e79dab25a937f254eebcf76aaf9d7f6582f
Elanrif <saidbacoelanrif@gmail.com>
2025-10-24 17:09:47 +0100

    feat: install lint via nextjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,17 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
-import { version } from 'react';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import prettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
+  ...nextVitals,
+  prettier,
   {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
     rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
       'no-unused-vars': 'error',
       'no-undef': 'error',
     },
-    settings: { react: { version: 'detect' } },
-    languageOptions: { globals: globals.browser },
   },
-  pluginReact.configs.flat.recommended,
+  globalIgnores(['.next/', 'out/**', 'build/', 'next-env.d.ts']),
 ]);
